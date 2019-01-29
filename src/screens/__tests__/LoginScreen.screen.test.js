5a98b3e72f643b3b1d8a83f343d013661a023d61
Teddy Morin <morin.td@gmail.com>
2019-01-29 13:49:30 +0100

    Les tests:screen passent

--- src/screens/__tests__/LoginScreen.screen.test.js
+++ src/screens/__tests__/LoginScreen.screen.test.js
@@ -2,13 +2,18 @@ import React from 'react';
 import LoginScreen from 'screens/LoginScreen';
 import renderer from 'react-test-renderer';
 import 'utils/__tests__/throwOnConsoleError';
+import 'utils/__tests__/mockNativeModules';
+import withStore from 'utils/__tests__/mockStore';
+
+jest.mock('react-navigation', () => ({ withNavigation: component => component }));
+const navigation = { navigate: jest.fn() };
 
 describe('<LoginScreen />', () => {
   test('it renders correctly', () => {
-    renderer.create(<LoginScreen />);
+    renderer.create(withStore(<LoginScreen navigation={navigation} />));
   });
   test('it renders correctly when register form is displayed', () => {
-    const component = renderer.create(<LoginScreen />);
+    const component = renderer.create(withStore(<LoginScreen navigation={navigation} />));
     const instance = component.root.instance;
     instance.setState({ form: 'register' });
   });
--- src/utils/__tests__/mockNavigation.js
+++
@@ -1,1 +0,0 @@
-export default { navigate: jest.fn() };
